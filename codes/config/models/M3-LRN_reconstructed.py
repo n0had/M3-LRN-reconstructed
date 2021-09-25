07cfc47e459a0c4f0ccd2e4aff8a3d7f2ce53893
n0had <89034489+n0had@users.noreply.github.com>
2021-09-25 23:41:16 +0300

    Update M3-LRN_reconstructed.py

--- codes/config/models/M3-LRN_reconstructed.py
+++ codes/config/models/M3-LRN_reconstructed.py
@@ -1,8 +1,6 @@
 
 import os
 
-from collections import OrderedDict
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -29,67 +27,17 @@ def class M3_LRN_model():
         
         self.optimizer = optim.SGD(self.network.parameters(), lr=0.08, momentum=0.9)
         
-        lr_scheduler.MultiStepLR_Restart(
-                            self.optimizer,
-                            train_opt["lr_steps"],
-                            restarts=train_opt["restarts"],
-                            weights=train_opt["restart_weights"],
-                            gamma=train_opt["lr_gamma"],
-                            clear_state=train_opt["clear_state"],
-                        )
+        self.LRscheduler=torch.optim.lr_scheduler.MultiStepLR(self.optimizer,milestones=[48,64], gamma=0.1)
+        
+        self.total_epoches=80
+        self.batch_size=1024
     
-
-        if self.is_train:
-            train_opt = opt["train"]
-            # self.init_model() # Not use init is OK, since Pytorch has its owen init (by default)
-            self.netG.train()
-
-            # optimizers
-            wd_G = train_opt["weight_decay_G"] if train_opt["weight_decay_G"] else 0
-            optim_restorer = []
-            optim_estimator = []
-            for (k, v) in self.netG.named_parameters():  # can optimize for a part of the model
-                if v.requires_grad:
-                    if "Restorer" in k:
-                        optim_restorer.append(v)
-                    elif "Estimator" in k:
-                        optim_estimator.append(v)
-                else:
-                    if self.rank <= 0:
-                        logger.warning("Params [{:s}] will not optimize.".format(k))
-                        
-                        
-            
-            
-            
-                [
-                    {"params": optim_restorer, "lr": train_opt["lr_G"]},
-                    {"params": optim_estimator, "lr": train_opt["lr_E"]},
-                ],
-                weight_decay=wd_G,
-                betas=(train_opt["beta1"], train_opt["beta2"]),
-            )
-            # self.optimizer_G = torch.optim.SGD(optim_params, lr=train_opt['lr_G'], momentum=0.9)
-            self.optimizers.append(self.optimizer_G)
-
-            # schedulers
-            if train_opt["lr_scheme"] == "MultiStepLR":
-                for optimizer in self.optimizers:
-                    self.schedulers.append(
-                        lr_scheduler.MultiStepLR_Restart(
-                            optimizer,
-                            train_opt["lr_steps"],
-                            restarts=train_opt["restarts"],
-                            weights=train_opt["restart_weights"],
-                            gamma=train_opt["lr_gamma"],
-                            clear_state=train_opt["clear_state"],
-                        )
-                    )
-            
-            else:
-                print("MultiStepLR learning rate scheme is enough.")
-
-            self.log_dict = OrderedDict()
+    
+    train_set = create_dataset(dataset_opt)
+            train_size = int(math.ceil(len(train_set) / dataset_opt["batch_size"]))
+            total_iters = int(opt["train"]["niter"])
+            total_epochs = int(math.ceil(total_iters / train_size))
+          
             
     def optimize_parameters(self, step):
         self.optimizer.zero_grad()
@@ -109,20 +57,7 @@ def class M3_LRN_model():
 
         total_loss.backward()
         self.optimizer.step()
-        
-        
-    def _set_lr(self, lr_groups_l):
-        """set learning rate for warmup,
-        lr_groups_l: list for lr_groups. each for a optimizer"""
-        for optimizer, lr_groups in zip(self.optimizers, lr_groups_l):
-            for param_group, lr in zip(optimizer.param_groups, lr_groups):
-                param_group["lr"] = lr
 
-    def _get_init_lr(self):
-        # get the initial lr, which is set by the scheduler
-        init_lr_groups_l = []
-        for optimizer in self.optimizers:
-            init_lr_groups_l.append([v["initial_lr"] for v in optimizer.param_groups])
-        return init_lr_groups_l
-
-    def update_learning_rate(self, cur_iter, warmup_iter=-1):
+    def train_network():
+      for epoch in range(1, self.total_epoches+1):
+        
