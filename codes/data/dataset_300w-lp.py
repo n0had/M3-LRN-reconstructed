41dc641dbc3a6e7471829f95830dce026026d954
n0had <89034489+n0had@users.noreply.github.com>
2021-09-27 00:09:18 +0300

    Update dataset_300w-lp.py

--- codes/data/dataset_300w-lp.py
+++ codes/data/dataset_300w-lp.py
@@ -6,16 +6,16 @@ import pandas as pd
 from torchvision.io import read_image
 
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+    def __init__(self, data_directory, img_dir, transform=None, target_transform=None):
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
 
-    def __len__(self):
-        return len(self.img_labels)
+    #def __len__(self):
+    #    return len(self.img_labels)
 
-    def __getitem__(self, idx):
+    def __getitem__(self): #idx
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
         image = read_image(img_path)
         label = self.img_labels.iloc[idx, 1]
@@ -23,4 +23,8 @@ class CustomImageDataset(Dataset):
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
-        return image, label
+        #return image, label
+        
+        yield os.path.basename(image_file), record
+    
+
